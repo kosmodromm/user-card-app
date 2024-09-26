91a9e6dc48c243145be10e304b161bb89b08f232
Anton Kizerau <a.kizerau@sam-solutions.com>
2024-09-26 19:52:34 +0200

    add app implementation

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { API } from './API.js';
+import { UserList } from './UserList.js';
+import { debounce } from './utils.js';
+import './styles.css';
+
+async function init() {
+  const users = await API.getUsers();
+  const userList = new UserList(users);
+  userList.render();
+  userList.createFilterButtons();
+
+  const filterInput = document.getElementById('filter-input');
+  const debouncedFilter = debounce((query) => userList.filter(query), 500);
+
+  filterInput.addEventListener('input', (e) => {
+    debouncedFilter(e.target.value);
+  });
+}
+
+init();
